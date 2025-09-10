7dfd33f2da16c6e3ad679e1661febf2b423b2b96
Nicholas Do <146508658+NicholasDo18@users.noreply.github.com.>
2025-09-10 12:05:08 -0400

    Create a quote command and randomly picks a motivational quote from Lebron's motivational list.

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
 import os
-
+import random
 import telebot 
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+leQuotes = ["I like criticism. It makes you strong.",
+"I think, team first. It allows me to succeed, it allows my team to succeed.",
+"Don’t be afraid of failure. This is the way to succeed.",
+"Commitment is a big part of what I am and what I believe. How committed are you to winning?",
+"I always say, decisions I make, I live with them. There’s always ways you can correct them or ways you can do them better.",
+"You can’t be afraid to fail. It’s the only way you succeed."]
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "King James said Hi")
+
+@bot.message_handler(commands = ['Motivational-quote'])
+def send_welcome(message):
+    bot.reply_to(message, random.choice(leQuotes))
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
