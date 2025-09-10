ce8c8545619b00632e027f671fd02632afb31828
Nicholas Do <146508658+NicholasDo18@users.noreply.github.com.>
2025-09-09 23:30:33 -0400

    "Add echo command to repeat sent messages")

--- bot.py
+++ bot.py
@@ -9,3 +9,9 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "King James said Hi")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+bot.infinity_polling()
